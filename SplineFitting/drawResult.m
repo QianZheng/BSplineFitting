17090d72abbd72984ae9d2eab7640137af101203
QianZheng <qian.ariel.zheng@gmail.com>
2014-08-06 12:47:27 +0800

    add three results

---
+++ SplineFitting/drawResult.m
@@ -0,0 +1,28 @@
+function  drawResult(input_name)
+
+control_name = strcat(input_name, '_controls.txt');
+spline_name = strcat(input_name, '_spline.txt');
+
+P = load(input_name);
+figure;
+plot(P(:,1), P(:,2),'b.','MarkerSize',15);
+
+hold on;
+C = load(control_name);
+C2 = [C; C(1,:)];
+plot(C2(:,1),C2(:,2),'r','LineWidth', 1);
+plot(C2(:,1),C2(:,2), 'rx', 'MarkerSize',10);
+
+
+S = load(spline_name);
+S2 = [S; S(1,:)];
+plot(S2(:,1),S2(:,2),'g','LineWidth', 2);
+end
+
+
+
+
+
+
+
+
